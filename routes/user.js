76e241da04867f47232aa6ae5556ad482089f50b
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2019-03-17 15:04:33 +0530

    Add friends and search API

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const router = express.Router();
+const router  = express.Router();
+const User    = require('./models/userModel');
 
 // Send user data (other than his uid), 
 // also send his logs data (excluding sensitive data) by iterating over his logs array.
@@ -7,9 +8,24 @@ router.get("/", (req, res, next) => {
     res.json(req.jwt);
 });
 
-// Search a user by prefix of a username
-router.get("/search/:usernamePrefix", (req, res, next) => {
-    let usernamePrefix = req.params.usernamePrefix;
+// Search a user's friend by prefix of a username
+router.get("/friendsearch/:friendPrefix", (req, res, next) => {
+    let friendPrefix = req.params.friendPrefix;
+    // Search user's friends by prefix in req.userFriends
+    res.send("Hello");
+});
+
+// Search a user's friend by prefix of a username
+router.get("/searchuser/:username", (req, res, next) => {
+    let friendPrefix = req.params.friendPrefix;
+    // Search user's friends by prefix in req.userFriends
+    res.send("Hello");
+});
+
+// Add user's friend
+router.get("/addfriend/:friendusername", (req, res, next) => {
+    let friendPrefix = req.params.friendPrefix;
+    // Search user's friends by prefix in req.userFriends
     res.send("Hello");
 });
 
