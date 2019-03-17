69b7867905d88cca3d86e1a240c9caaa125f3414
Tanmay Anand <anand44tanmay@gmail.com>
2019-03-17 18:36:53 +0530

    Delete logs

--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const User = require("../models/userModel");
+const Logs = require("../models/logModel");
 // Send user data (other than his uid),
 // also send his logs data (excluding sensitive data) by iterating over his logs array.
+
+
 router.get("/", (req, res, next) => {
     // console.log("hello");
-    User.find({"uid" : req.jwt.claims.uid}).exec().then((result)=>{
-      var userJson = result[0];
-      delete userJson.uid;//not working
-      console.log(userJson.uid);
+    User.find({"uid" : req.jwt.claims.uid}).then((result)=>{
+      userJson = result[0];
+      userJson.uid=undefined;//delete is not working also delete is 100 times slower as compared to setting undefined
       res.json(userJson);
     });
 });
@@ -17,6 +19,13 @@ router.get("/", (req, res, next) => {
 // Delete a user's log if it's present else do nothing
 router.get("/logs/delete/:uniqRefId", (req, res, next) => {
     var uniqRefId = req.params.uniqueRefId;
+    for (var i in req.userLogs){
+      if(req.userLogs[i].uniqRefId == uniqRefId){
+        Logs.deleteOne({
+          "uniqRefId": uniqRefId
+        });
+      }
+    }
     res.json(logId);
 });
 
