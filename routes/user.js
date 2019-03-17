e5e6b3aced6d53ac6a7f10f69f7b85adf5facfce
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2019-03-17 13:57:03 +0530

    Declare search user route

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,12 @@ router.get("/", (req, res, next) => {
     res.json(req.jwt);
 });
 
+// Search a user by prefix of a username
+router.get("/search/:usernamePrefix", (req, res, next) => {
+    let usernamePrefix = req.params.usernamePrefix;
+    res.send("Hello");
+});
+
 // Delete a user's log if it's present else do nothing
 router.get("/logs/delete/:uniqRefId", (req, res, next) => {
     var uniqRefId = req.params.uniqueRefId;
