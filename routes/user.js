6bf1ec077b3896778053bca2a6d3b39af729a00c
Tanmay Anand <anand44tanmay@gmail.com>
2019-03-17 14:49:21 +0530

    user data

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
+const mongoose = require("mongoose");
+const User = require("../models/userModel");
 // Send user data (other than his uid),
 // also send his logs data (excluding sensitive data) by iterating over his logs array.
 router.get("/", (req, res, next) => {
     // console.log("hello");
-    userJson=req.jwt;
-    delete userJson.claims.uid;
-    res.json(userJson);
+    User.find({"uid" : req.jwt.claims.uid}).exec().then((result)=>{
+      var userJson = result[0];
+      delete userJson.uid;//not working
+      console.log(userJson.uid);
+      res.json(userJson);
+    });
 });
 
 // Delete a user's log if it's present else do nothing
