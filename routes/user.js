85689a4a9db5b3a5241e03956a19b79705c8ef24
Tanmay Anand <anand44tanmay@gmail.com>
2019-03-17 13:50:40 +0530

    User data endpoint excluding uid

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-// Send user data (other than his uid), 
+// Send user data (other than his uid),
 // also send his logs data (excluding sensitive data) by iterating over his logs array.
 router.get("/", (req, res, next) => {
-    res.json(req.jwt);
+    // console.log("hello");
+    userJson=req.jwt;
+    delete userJson.claims.uid;
+    res.json(userJson);
 });
 
 // Delete a user's log if it's present else do nothing
