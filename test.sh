ecc340d098a9d57ba227a1b0bbe3f2c8dbd3a4a0
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2019-03-18 17:53:41 +0530

    Add tests in test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
 # Replace this accessToken with refreshed one
-accessToken="eyJraWQiOiJjcFF2WUpNaElhM1dBNFN2Zy1pSGdtdHJXRndVX2V1WHFsTVpsU3FWSXVrIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULm9pb0lUa2lWRlpadGM5aHZJSEJVUEZ5dUVCR3JIMzI0aEkxcmZNcHRUdE0iLCJpc3MiOiJodHRwczovL2Rldi03MTIzMjEub2t0YXByZXZpZXcuY29tL29hdXRoMi9kZWZhdWx0IiwiYXVkIjoiYXBpOi8vZGVmYXVsdCIsImlhdCI6MTU1Mjc3MDUyMCwiZXhwIjoxNTUyNzc0MTIwLCJjaWQiOiIwb2FqcjdnazE3NUN6Q1dCcjBoNyIsInVpZCI6IjAwdWpxcDJmeHhwWWh5U3NBMGg3Iiwic2NwIjpbIm9wZW5pZCIsImVtYWlsIl0sInN1YiI6InZhaWJoYXYudGhha2thci4yMi4xMi45OUBnbWFpbC5jb20ifQ.HKDPyIT6CE2eMDd9DiwuPY6CrT9XoxXKeEL6VnwKWvcUniC1AT-TXFdS06V-0Cdj-Dv-xLVYDLaoeiypVMGvGEm7CaFgh8aSp1TJ2dFbC96cBpbdIEwv_GyeKW6DTG1QsmhAY1UGMXbHu_BfoZzX0E0Ai_ldPn6E4ahOaYQ4qF4uGTJnuWmeeInU1h0380jkOLcuoucJQl2GPpJatEkzRRT5vCJyXADsvDDZZjxoUMigngXFLS21cyJ3xXla7kJewfby4VCXMomO5xggP1tS7-b9oa0QrjqABg7bKJVcjO50rb3PA6tyYUo9-yPfte11zfbjxaA_qI3X6l7wDo6u1g"
+accessToken="eyJraWQiOiJjcFF2WUpNaElhM1dBNFN2Zy1pSGdtdHJXRndVX2V1WHFsTVpsU3FWSXVrIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULnN2LXNzbXhFOE5mUURRR2hhZWM0aVdWTm9Rd1ZCUXlINkVkS3BRYThfdlUiLCJpc3MiOiJodHRwczovL2Rldi03MTIzMjEub2t0YXByZXZpZXcuY29tL29hdXRoMi9kZWZhdWx0IiwiYXVkIjoiYXBpOi8vZGVmYXVsdCIsImlhdCI6MTU1MjkxMTE0OSwiZXhwIjoxNTUyOTE0NzQ5LCJjaWQiOiIwb2FqcjdnazE3NUN6Q1dCcjBoNyIsInVpZCI6IjAwdWpxcDJmeHhwWWh5U3NBMGg3Iiwic2NwIjpbIm9wZW5pZCIsImVtYWlsIl0sInN1YiI6InZhaWJoYXYudGhha2thci4yMi4xMi45OUBnbWFpbC5jb20ifQ.lD3htT26bUyzRwcorwpVN-1eoYVQIuAWtmbFftgA4QDcfxYeJ3Ek8IwQbv0Q5GSiCL45Vjr30tR9bnIrdfz5qeao9b_OujqhtcFymCPBuxtkoGh9XTAmRdJdrdpcuFnMfFvAm6VtUec4Fqr7piV-Hc26YlNvBGHwXBH9vd6_rzA_iNQRoLxHuM4YWbLUXIpHisbAaSYISx38f5g9skM4Jix-piRCXdSHVl1zrK-1HVxhyYEA0ZlTxyvGhLZtYi6t6Nj5iiU-8g9zVbeu1kDVAhIM9kQBSfxS0XrdHULto_p4Q8045Xt-VbHZVPn54Q10NmDxtOYZhjjK6Vifj9PgDg"
 
-request="http get http://localhost:8080/api/v1/user 'Authorization: \"Bearer $accessToken\"' -v --ignore-stdin"
+# Get user dashboard data
+request="http get http://localhost:8080/api/v1/user/ 'Authorization: \"Bearer $accessToken\"' -v --ignore-stdin"
+eval $request
+
+# Search existing user
+request="http get http://localhost:8080/api/v1/user/searchuser?uname=vaibhav.thakkar.22.12.99@gmail.com 'Authorization: \"Bearer $accessToken\"' -v --ignore-stdin"
+eval $request
+
+# Search non-existing user
+request="http get http://localhost:8080/api/v1/user/searchuser?uname=random_name 'Authorization: \"Bearer $accessToken\"' -v --ignore-stdin"
+eval $request
+
+# Add non existing user as friend
+request="http get http://localhost:8080/api/v1/user/addfriend?uname=random_name 'Authorization: \"Bearer $accessToken\"' -v --ignore-stdin"
+eval $request
+
+# Add existing user as friend
+request="http get http://localhost:8080/api/v1/user/addfriend?uname=anand44tanmay@gmail.com 'Authorization: \"Bearer $accessToken\"' -v --ignore-stdin"
+eval $request
+
+# Adding new log
+msg="Testing log data"
+request="http --form POST http://localhost:8080/api/v1/logs/new msg='$msg' 'Authorization: \"Bearer $accessToken\"' -v --ignore-stdin"
 eval $request
