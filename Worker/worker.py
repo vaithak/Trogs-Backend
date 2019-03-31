fa23afdc30512695c3da380eef49ac8845c3a95d
Tanmay Anand <anand44tanmay@gmail.com>
2019-03-31 16:23:47 +0530

    removed useless codev2

--- Worker/worker.py
+++ Worker/worker.py
@@ -11,23 +11,6 @@ from elasticsearch import Elasticsearch
 config = configparser.ConfigParser()
 config.read('config.ini')
 es=Elasticsearch();
-# doc = {
-#     'author': 'kimchy',
-#     'text': 'Elasticsearch: cool. bonsai cool.',
-#     'timestamp': 'j',
-# }
-# res = es.index(index="test-index", doc_type='tweet', id=1, body=doc)
-# print(res['result'])
-#
-#
-# # res = es.get(index="test-index", doc_type='tweet', id=1)
-# # print(res['_source'])
-#
-#
-# res = es.search(index="test-index", body={"query": {"match_all": {}}})
-# print("Got %d Hits:" % res['hits']['total'])
-# for hit in res['hits']['hits']:
-#     print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
 
 
 # Handling JSON data received in request
